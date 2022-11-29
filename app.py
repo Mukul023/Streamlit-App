ffc1aa50764e1a21493bbfbace6db5ac7231f6c8
Mritunjay Pargotra <mukulpargotra27@gmail.com>
2022-11-28 20:46:52 -0500

    initial commits

--- app.py
+++ app.py
@@ -15,7 +15,6 @@ def load_lottieurl(url):
 def local_css(file_name):
     with open(file_name) as f:
         st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html = True)
-
 local_css("style/style.css")
 
 #---- LOAD ASSETS ----
